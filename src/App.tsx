76c9f1aaed16aa49286aa6fcad406f77b17aa008
Emart99 <ezequielsimon22@yahoo.com.ar>
2025-05-22 17:10:20 -0300

    changes to title

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
   return (
     <HelmetProvider>
       <Helmet>
-        <title>Ezequiel Martino | Web Developer</title>
+        <title>Ezequiel Martino</title>
         <meta name="description" content="Portfolio of Ezequiel Martino, a web developer specialized in React, TailwindCSS, Java and Springboot." />
         <meta name="keywords" content="Ezequiel Martino, web developer, React, TailwindCSS, Java, microservices, portfolio, programming" />
         <meta name="author" content="Ezequiel Martino" />
